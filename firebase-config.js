a365b6989366a9934c97a7743073eef834709307
clarkeblood1-cmd <clarkeblood1@gmail.com>
2026-03-24 03:06:38 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,6 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyBXqoG8ZaDbuY-KJwS5ClMovgy8Pb3vw54",
+  authDomain: "psykopatkontroll.firebaseapp.com",
+  projectId: "psykopatkontroll",
+  appId: "1:89089537506:web:573383e0e2ebf4ded035bb"
+};
